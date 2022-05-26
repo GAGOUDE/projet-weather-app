06844d3149a6316844dd88037556829c80ffc4f2
Gaël-Freddy GAGOUDE <gael.gagoude@gmail.com>
2022-05-26 14:24:08 +0200

    backend connexion

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var mongoose = require('./models/connection')
+var mongoose = require('./models/connection');
 
 var session = require("express-session");
 
--- models/connection.js
+++ models/connection.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
 
+const dotenv = require("dotenv");
+dotenv.config();
+
+const BDD_URL = process.env.BDD_URL;
+
 const options = {
     connectTimeoutMS: 5000,
     useUnifiedTopology : true,
     useNewUrlParser: true,
 }
 
-mongoose.connect('mongodb+srv://weather_App1:weather_App1_Portfolio@cluster0.xzqpz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect(BDD_URL,
     options,
     function (err) {
         console.log(err);
